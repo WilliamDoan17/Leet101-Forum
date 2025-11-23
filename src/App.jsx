a4105607f518f9a686d29f369c7607d148fe7055
WilliamDoan17 <pdoanthe96@gmail.com>
2025-11-23 18:19:08 -0500

    Configed some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header'
+
+const TestElement = () => {
+  return (
+    <>
+      <Header>
+      </Header>
+    </>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <TestElement>
+      </TestElement>
     </>
   )
 }
