14e2ae9e4485c49611653c33c63a318e494eb47b
WilliamDoan17 <pdoanthe96@gmail.com>
2025-11-23 18:35:08 -0500

    app-info in header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,29 @@
 import styles from './Header.module.css'
 
 const Header = () => {
+    const author = 'Phong Doan';
+    const appName = 'Leet101';
+
     return (
         <>
-            
+            <div
+                className = {`${styles.container}`}
+            >
+                <div
+                    className = {`${styles['app-info']}`}
+                >
+                    <p
+                        className = {`${styles['app-name']}`}
+                    >
+                        {appName}
+                    </p>
+                    <p
+                        className = {`${styles['app-author']}`}
+                    >
+                        by {author}
+                    </p>
+                </div>
+            </div>
         </>
     )
 }
