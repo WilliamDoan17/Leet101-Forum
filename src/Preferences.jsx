6d58a19625d8d49e5b766b080cee7e8979a02011
WilliamDoan17 <pdoanthe96@gmail.com>
2025-11-23 21:41:55 -0500

    added preference: search by keyword

--- src/Preferences.jsx
+++ src/Preferences.jsx
@@ -2,14 +2,42 @@ import { useState } from 'react'
 import styles from './Preferences.module.css'
 
 const KeywordPreference = () => {
-    const [keyword, setKeyword] = useState('');
+    const [keywordInput, setKeywordInput] = useState('');
+
+    const handleKeywordChange = (e) => {
+        setKeywordInput(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
 
     return (
         <>
             <div
                 className = {`${styles['preference-container']}`} 
             >
-                
+                <form
+                    onSubmit = {handleSubmit}
+                >
+                    <label
+                        className = {`${styles['keyword-label']}`}
+                        htmlFor = 'keyword'
+
+                    >
+                        Search by keyword
+                    </label>
+                    <input
+                        className = {`${styles['keyword-input']}`}
+                        id = 'keyword'
+                        type = "text"
+                        name = 'keyword'
+                        placeholder = 'Input keyword'
+                        value = {keywordInput}
+                        onChange = {handleKeywordChange}
+                    >
+                    </input>
+                </form>
             </div>
         </>
     )
@@ -29,6 +57,8 @@ const Preferences = () => {
                     >
                         Adjust your preference of questions below to access solutions for your Leet101 questions!
                     </p>
+                    <KeywordPreference>
+                    </KeywordPreference>
                 </div>
             </div>
         </>
